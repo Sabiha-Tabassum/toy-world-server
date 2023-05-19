8c514d75ddbadec7d00c8df202efac496d23ad90
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-05-20 00:46:18 +0600

    third commit

--- index.js
+++ index.js
@@ -27,12 +27,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db('toyWorld').collection('collection');
+    const addToyCollection = client.db('toyWorld').collection('addToy');
+
+    app.get('/collection', async(req,res) => {
+        const cursor = toyCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+
+    })
+
+    app.post('/addToy', async(req,res) => {
+          const addingToy = req.body;
+          console.log(addingToy);
+          const result = await addToyCollection.insertOne(addingToy);
+          res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
