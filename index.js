b9f0f562b44856956abad35a55e62622f1490d74
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-05-20 11:35:55 +0600

    nine commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -60,6 +60,13 @@ async function run() {
         }
         const result = await addToyCollection.find().toArray();
         res.send(result);
+    })
+
+    app.get('/collection/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await toyCollection.findOne(query);
+      res.send(result);
     })
 
     // app.get('/collection/:text', async(req,res) => {
